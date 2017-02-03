7a269d9bdfd982d0dc989b8cb96d7c1d176a7262
Khrystyna <khrystynkakitty@gmail.com>
2017-02-03 12:09:05 +0000

    text

---
+++ index.js
@@ -0,0 +1,9 @@
+var planet =  document.getElementById("movie1");
+var code = planet.innerHTML;
+alert(code);
+
+
+var planet2 =  document.getElementById("kla");
+planet.innerHTML = "dlkjglkfjg";
+
+
